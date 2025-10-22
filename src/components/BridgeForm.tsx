6a1b5d0f09163b9afed8dad906d78f1813a72fb9
Benjamin Odiowa <benco00@yahoo.com>
2025-10-22 12:27:46 +0000

    Update finalRecipientAddress assignment logic

--- src/components/BridgeForm.tsx
+++ src/components/BridgeForm.tsx
@@ -250,7 +250,7 @@ export default function BridgeForm() {
       finalTokenAddress = isNativeWithdrawal
         ? "0x0000000000000000000000000000000000000000"
         : hederaTokenCheckSum
-      finalRecipientAddress = convertHederaIdToEVMAddress(userToAddress)
+      finalRecipientAddress = userToAddress
     } else {
       finalContractAddress = CONTRACT_ADDRESSES[toNetwork]
       finalTokenAddress = isNativeWithdrawal
